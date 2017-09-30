a984881f31181579d3d1476c0de40a625d9a6b99
Ash R <ashwinramki@gmail.com>
2017-09-30 10:57:56 -0500

    occurrence.c done

--- lab6/occurrence.c
+++ lab6/occurrence.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(void)
 {
     int n;
+    int array[10] = {0}; 
     while ( (n = getchar()) != EOF )
     {
-        frequency(n);
+        n = n - '0' ;
+        if ( ( n >= 0 ) && ( n <= 9 ) )
+        {
+            array[n] = array[n] + 1 ;
+        }
     }
+
+    printf("\n");
+    for ( int i =0 ; i < 10 ; i ++ )
+    {
+        printf("%d     %d\n", i, array[i]) ;
+    }
+    return 0 ; 
 }
 
