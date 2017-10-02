35543f03cfa50795c4bd4006fdb8f9d81edcbb32
Ash R <ashwinramki@gmail.com>
2017-10-02 00:46:44 -0500

    richard sort now supports both positive and negative values

--- unsw-1927/lab0/richard_sort.c
+++ unsw-1927/lab0/richard_sort.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 
-#define MAXLENGTH 655360
+//#define MAXLENGTH 2096450  // Max length my raspberry pi supports :( .. damn you memory
+//#define PIVOT (2096450/2) // midpoint ; so that we can store both negative and positive values
+#define MAXLENGTH 2096000  // Max length my raspberry pi supports :( .. damn you memory
+#define PIVOT (2096000/2) // midpoint ; so that we can store both negative and postive values
 
 
 int main ( void )
@@ -10,14 +13,14 @@ int main ( void )
     while ( (scanf("%d", &temp) ) != EOF )
     {
         //printf("temp = %d ; array[temp] = %d\n", temp, array[temp]);
-        array[temp] ++ ; 
+        array[temp + PIVOT] ++ ;  
     }
     for ( int i = 0 ; i < MAXLENGTH ; i ++ )
     {
         //printf("i is %d ; array[i] is %d \n", i, array[i];
         while (array[i] != 0)
         {
-            printf("%d\n", i);
+            printf("%d\n", i - PIVOT);
             array[i] -- ;
         }
     }
